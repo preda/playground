9fb263b08a302f2dd843cbe8aa27eb76f5608e66
Mihai Preda <mpreda@gmail.com>
2015-05-01 22:20:27 +1000

    testing hasFactor two variants

--- tf/test.cu
+++ tf/test.cu
@@ -12,6 +12,7 @@ typedef __uint128_t u128;
 
 struct U3 { unsigned a, b, c; };
 struct U4 { unsigned a, b, c, d; };
+struct U5 { unsigned a, b, c, d, e; };
 struct U6 { unsigned a, b, c, d, e, f; };
 
 #define __both__ __device__ __host__
@@ -77,6 +78,22 @@ __device__ static U4 sub(U4 x, U4 y) {
   return {a, b, c, d};
 }
 
+__device__ static U5 sub(U5 x, U5 y) {
+  unsigned a, b, c, d, e, carryOut;
+  asm("sub.cc.u32  %0, %6,   %11;"
+      "subc.cc.u32 %1, %7,   %12;"
+      "subc.cc.u32 %2, %8,   %13;"
+      "subc.cc.u32 %3, %9,   %14;"
+      "subc.cc.u32 %4, %10,  %15;"
+      "subc.u32    %5, 0, 0;"
+      : "=r"(a), "=r"(b), "=r"(c), "=r"(d), "=r"(e), "=r"(carryOut)
+      : "r"(x.a), "r"(x.b), "r"(x.c), "r"(x.d), "r"(x.e),
+        "r"(y.a), "r"(y.b), "r"(y.c), "r"(y.d), "r"(y.e));
+  assert(!carryOut);
+  return {a, b, c, d, e};
+}
+
+/*
 __device__ static U6 subShl2w(U6 x, U4 y) {
   U4 r = sub((U4){x.c, x.d, x.e, x.f}, y);
   return {x.a, x.b, r.a, r.b, r.c, r.d};
@@ -98,6 +115,7 @@ __device__ static U6 sub(U6 x, U6 y) {
   assert(!carryOut);
   return {a, b, c, d, e, f};
 }
+*/
 
 __device__ static U4 mul(U3 x, unsigned n) {
   unsigned a, b, c, d;
@@ -137,10 +155,19 @@ __device__ static U6 square(U3 x) {
   return r;
 }
 
-__device__ static U6 shl1w(U4 x)  { return {0, x.a, x.b, x.c, x.d, 0}; }
+// __device__ static U6 shl1w(U4 x)  { return {0, x.a, x.b, x.c, x.d, 0}; }
+__device__ static U5 shl1w(U4 x)  { return {0, x.a, x.b, x.c, x.d}; }
 __device__ static U6 shl2w(U4 x)  { return {0, 0, x.a, x.b, x.c, x.d}; }
+__device__ static U6 makeU6(U3 x) { return {x.a, x.b, x.c, 0, 0, 0}; }
 __device__ static U6 makeU6(U4 x) { return {x.a, x.b, x.c, x.d, 0, 0}; }
+__device__ static U6 makeU6(U5 x) { return {x.a, x.b, x.c, x.d, x.e, 0}; }
+__device__ static U5 makeU5(U4 x) { return {x.a, x.b, x.c, x.d, 0}; }
 __device__ static U4 makeU4(U3 x) { return {x.a, x.b, x.c, 0}; }
+
+__host__ static U3 makeU3(u128 x) {
+  assert(!(unsigned) (x >> 96));
+  return { (unsigned) x, (unsigned) (x >> 32), (unsigned) (x >> 64)};
+}
 
 __device__ static U3 shl(U3 x, int n) {
   assert(n >= 0 && n < 32 && !(x.c >> (32 - n)));
@@ -148,8 +175,14 @@ __device__ static U3 shl(U3 x, int n) {
 }
 
 __device__ static U4 shl(U4 x, int n) {
+  // printf("shl %d ", n); print(x);
   assert(n >= 0 && n < 32 && !(x.d >> (32 - n)));
   return {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n), shl(x.c, x.d, n)};
+}
+
+__device__ static U5 shl(U5 x, int n) {
+  assert(n >= 0 && n < 32 && !(x.e >> (32 - n)));
+  return {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n), shl(x.c, x.d, n), shl(x.d, x.e, n)};
 }
 
 __device__ static U6 shl(U6 x, int n) {
@@ -157,40 +190,47 @@ __device__ static U6 shl(U6 x, int n) {
   return {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n), shl(x.c, x.d, n), shl(x.d, x.e, n), shl(x.e, x.f, n)};
 }
 
+/*
+__device__ static U6 shl(U6 x, int n) {
+  assert(n >= 0 && n < 32 && !(x.f >> (32 - n)));
+  return {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n), shl(x.c, x.d, n), shl(x.d, x.e, n), shl(x.e, x.f, n)};
+}
+*/
+
 __device__ static U3 shr(U3 x, int n) {
   assert(n >= 0 && n < 32 && !(x.a << (32 - n)));
   return {shr(x.a, x.b, n), shr(x.b, x.c, n), x.c >> n};
 }
 
 // m >= 2^93 && m < 2^94.
-__device__ __noinline__ U3 mod(U6 x, U3 m, int shift, unsigned R) {
+__device__ __noinline__ U3 mod(U4 xx, U3 m, int shift, unsigned R) {
   unsigned n;
-  
   assert((m.c >> 29) == 1);
+  U5 x = makeU5(xx);
   x = shl(x, shift);
-  n = mulhi(x.f, R);
+  
+  n = mulhi(x.e, R);  
+  x = sub(x, shl(shl1w(mul(m, n)), 3));
+  assert(!(x.e & 0xfffffff0));
+  
+  n = mulhi(shl(x.d, x.e, 28), R);
+  x = sub(x, shl(makeU5(mul(m, n)), 7));
+  assert(!x.e && !(x.d & 0xffffff00));
+
+  n = mulhi(shl(x.c, x.d, 24), R) >> 21;
+  x = sub(x, makeU5(mul(m, n)));
+  assert(!x.e && !x.d);
+  
   /*
-  print(x);
-  U4 tmp2 = shl(mul(m, n), 3);
-  print(tmp2);
-  x = subShl2w(x, tmp2);
+  n = mulhi(x.d, R);  
+  x = sub(x, shl(mul(m, n), 3));
+  assert(!(x.d & 0xfffffff0));
+  n = mulhi(shl(x.c, x.d, 28), R) >> 25;
+  x = sub(x, mul(m, n));
+  assert(!x.d);
   */
-  U6 tmp = shl2w(shl(mul(m, n), 3));
-  print(tmp);
-  x = sub(x, tmp);
-  print(x);
-  assert(!(x.f & 0xfffffff0));
-  n = mulhi(shl(x.e, x.f, 28), R);
-  x = sub(x, shl(shl1w(mul(m, n)), 7));
-  assert(!x.f && !(x.e & 0xffffff00));
-  n = mulhi(shl(x.d, x.e, 24), R);
-  x = sub(x, shl(makeU6(mul(m, n)), 11));
-  assert(!x.f && !x.e && !(x.d & 0xfffff000));
-  n = mulhi(shl(x.c, x.d, 20), R) >> 17;
-  x = sub(x, makeU6(mul(m, n)));
-  assert(!x.f && !x.e && !x.d);  
-  U3 r{x.a, x.b, x.c};  
-  return shr(r, shift);
+  
+  return shr((U3) {x.a, x.b, x.c}, shift);
 }
 
 // Compute mp0 such that: (unsigned) (m * mp0) == 0xffffffff; using variant extended euclidian algorithm.
@@ -216,7 +256,7 @@ __device__ static U3 montRed(U6 x, U3 m, unsigned mp0) {
   assert(!x.a);
   t = x.b * mp0;
   f = mul(m, t);
-  x = add(x, shl1w(f));
+  x = add(x, makeU6(shl1w(f)));
   assert(!x.b);
   t = x.c * mp0;
   f = mul(m, t);
@@ -230,6 +270,7 @@ __device__ static U3 montRed(U6 x, U3 m, unsigned mp0) {
 __device__ static U3 power32(U3 a, U3 m, unsigned mp0) {
   for (int i = 0; i < 5; ++i) {
     a = montRed(square(a), m, mp0);
+    // print(montRed(makeU6(a), m, mp0));
   }
   return a;
 }
@@ -237,6 +278,24 @@ __device__ static U3 power32(U3 a, U3 m, unsigned mp0) {
 // return x < y;
 __device__ static bool less(U3 x, U3 y) {
   return x.c < y.c || x.b < y.b || x.a < y.a;
+}
+
+__device__ static U3 hasFactor2(unsigned p, U3 m) {
+  unsigned mp0 = mprime0(m);
+  int mShift = __clz(m.c) - 2;
+  assert(mShift >= 0);
+  U3 flushedM = shl(m, mShift);
+  unsigned R = 0xffffffffffffffffULL / ((0x100000000ULL | shl(flushedM.b, flushedM.c, 3)) + 1);
+  U3 a = mod((U4){0, 0, 0, 1}, flushedM, mShift, R);
+  print(a);
+  for (int i = 0; i < 32; ++i) {
+    U6 a2 = square(a);
+    a = montRed(a2, m, mp0);
+    if (p & 0x80000000) { a = shl(a, 1); }
+    // printf("%2d: ", i); print(a);
+    p <<= 1;
+  }
+  return montRed(makeU6(a), m, mp0);
 }
 
 // Return whether m is a factor of 2^power - 1
@@ -251,14 +310,20 @@ __device__ static bool hasFactor(unsigned p, U3 m) {
   assert(mShift >= 0);
   U3 flushedM = shl(m, mShift);
   unsigned R = 0xffffffffffffffffULL / ((0x100000000ULL | shl(flushedM.b, flushedM.c, 3)) + 1);
-  
-  U3 a = mod((U6) {0, 0, 0, 1 << (p >> 27), 0, 0}, flushedM, mShift, R);
-  p <<= 5;
 
+  printf("p %d shift %d\n", (p >> 27), mShift);
+  U3 a = mod((U4) {0, 0, 0, 1 << (p >> 27)}, flushedM, mShift, R);
+  print(a);
   unsigned mp0 = mprime0(m);
+  print(montRed(makeU6(a), m, mp0));
+  
+  p <<= 5;
   for (int i = 0; i < 4; ++i, p <<= 5) {
+    printf("starting slice %d\n", i);
     a = power32(a, m, mp0);
-    a = mod(makeU6(shl(makeU4(a), p >> 27)), flushedM, mShift, R);
+    a = mod(shl(makeU4(a), p >> 27), flushedM, mShift, R);
+    print(a);
+    // print(montRed(makeU6(a), m, mp0));
   }
   
   a = power32(a, m, mp0);
@@ -267,26 +332,43 @@ __device__ static bool hasFactor(unsigned p, U3 m) {
   return a.a == 1 && !a.b && !a.c;
 }
 
-__global__ void test1(unsigned *out, unsigned p, U3 m) {
-  *out = hasFactor(p, m);
+__managed__ U3 out;
+
+__global__ void test(unsigned p, U3 m) {
+  out = hasFactor2(p, m);
 }
 
-#define N 32
+struct Test {
+  unsigned p;
+  u64 k;
+};
 
-__managed__ U6 as[N];
-__managed__ U3 bs[N];
-__managed__ U3 out[N * N];
-__managed__ unsigned out2[N];
+Test tests[] = {
+  {800000479,      3089082494924ull},
+  {800000011,       955047115584ull},
+  {500953,    895189023449202923ull},
+  {119129573,     12186227586967ull},
+};
 
 int main() {
   cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
-  U3 m{0xf817da27, 0x65e1be70, 0x0000009d};
-  test1<<<1, 1>>>(out2, 119129573, m);
-  cudaDeviceSynchronize();
-  cudaError_t err = cudaGetLastError();
-  if (err != cudaSuccess) {
-    printf("CUDA error: %s\n", cudaGetErrorString(err));
-  } else {
-    printf("hasFactor %d\n", out2[0]);
+  for (Test t : tests) {
+    unsigned p = t.p;
+    u64 k = t.k;
+    u128 mm = (2 * p) * (u128) k + 1;
+    U3 m = makeU3(mm);
+    // printf("%uL\n", k);
+    print(m);
+    test<<<1, 1>>>(p, m);
+    cudaDeviceSynchronize();
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+      printf("CUDA error: %s\n", cudaGetErrorString(err));
+      break;
+    } else {
+      print(out);
+    }
   }
 }
+
+  // U3 m{0xf817da27, 0x65e1be70, 0x0000009d};
--- tf/test.py
+++ tf/test.py
@@ -1,3 +1,28 @@
+from math import log
+
+k = 3089082494924
+p = 800000479
+a = 1
+m = 2 * k * p + 1
+print "m %x p %x" % (m, p)
+bit = int(log(p, 2))
+U = (1 << 96) % m
+
+while bit >= 0:
+    on = p & (1<<bit)
+    print "bit %d %d" % (bit, on)
+    a = (a * a) % m
+    if on: a = (a << 1) % m
+
+    b = (a << 96) % m
+    
+    print '%x - %x'% (a, b)
+    bit -= 1
+print a
+
+
+
+
 def mprime(m):
     m = (m >> 1) + 1
     u = m
@@ -14,22 +39,19 @@ def mprime(m):
 def reduct(x, m, mp):
     return (x + ((x * mp) & 0xffffffff) * m) >> 32
 
-
-
-
 n = 2903480177832398150183
 
-print "0x%08x, 0x%08x, 0x%08x" % (n >> 64, (n >> 32) & 0xffffffff, n & 0xffffffff) 
+#print "0x%08x, 0x%08x, 0x%08x" % (n >> 64, (n >> 32) & 0xffffffff, n & 0xffffffff) 
 
-for m in (41, 0xff, 0xffffffff, 1023, 955):
-    mp = mprime(m)
-    print "%x %x %x" % (m, mp, m * mp)
+#for m in (41, 0xff, 0xffffffff, 1023, 955):
+#    mp = mprime(m)
+#    print "%x %x %x" % (m, mp, m * mp)
 
-    for a in (1, 2, 24, 25, 10342):
-        a = a % m
-        aa = (a << 32) % m
-        a2a = reduct(aa * aa, m, mp)
-        r = reduct((aa + m) * (aa + m), m, mp)
+#    for a in (1, 2, 24, 25, 10342):
+#        a = a % m
+#        aa = (a << 32) % m
+#        a2a = reduct(aa * aa, m, mp)
+#        r = reduct((aa + m) * (aa + m), m, mp)
         #print a, reduct(aa, m, mp), (a*a) % m, reduct(a2a, m, mp), a2a % m, r % m
 
 
