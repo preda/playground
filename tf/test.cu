ef71e11faac6774f668d11a4c3a34e4e52256e51
Mihai Preda <mhpreda@gmail.com>
2015-06-07 21:58:27 +1000

    add some comments. remove some unused.

--- tf/test.cu
+++ tf/test.cu
@@ -1,5 +1,28 @@
 // Copyright (c) Mihai Preda, 2015.
 
+/*
+  This is a program for trial factoring of Mersenne numbers, which are numbers of the form
+  2^exp - 1. See http://www.mersenne.org/various/math.php
+
+  For a given mersenne number 2^exp-1, i.e. for a given value "exp", exp being prime,
+  the trial factors are of the form q = 2*k*exp + 1, and we're interested only in prime factors.
+
+  Range: exp < 2^30. q < 2^94.
+
+  A first step consist in generating prime candidate factors -- this is called "sieving" because
+  it uses Erathostene's sieve. Next each factor q is tested by computing "modular exponentiation",
+  reminder r = 2^exp modulo q. If this reminder is equal to 1, it means that q is a factor of
+  2^exp-1, and thus the mersenne number is not prime.
+
+  Both the sieving and the testing is run on the GPU.
+
+  Some naming conventions used:
+  shl: shift left.
+  shr: shift right.
+  U3: unsigned int using 3 words (i.e. 96bits).
+  u32: unsigned int using 32 bits.
+ */
+
 #include <stdio.h>
 #include <assert.h>
 #include <sys/time.h>
@@ -9,6 +32,8 @@ typedef unsigned u32;
 typedef unsigned long long u64;
 typedef __uint128_t u128;
 
+// Multi-precision unsigned ints with the given number of words.
+// The least-significant word is "a".
 struct U2 { u32 a, b; };
 struct U3 { u32 a, b, c; };
 struct U4 { u32 a, b, c, d; };
@@ -16,27 +41,40 @@ struct U5 { u32 a, b, c, d, e; };
 struct U6 { u32 a, b, c, d, e, f; };
 
 #define ASIZE(a) (sizeof(a) / sizeof(a[0]))
+
 #define THREADS_PER_BLOCK 512
+// How many threads do sieving, out of THREADS_PER_BLOCK.
+#define SIEVE_THREADS 32
+
+// How many words of shared memory to use for sieving.
+#define NWORDS (12 * 1024)
+
+// Must update acceptClass() when changing these.
 #define NCLASS     (4 * 3 * 5 * 7 * 11)
+
+// Derived values below.
+// How many threads do "modular exponentiation" to test factor candidates.
+#define WORK_THREADS (THREADS_PER_BLOCK - SIEVE_THREADS)
+// Bits for sieving.
+#define NBITS (NWORDS << 5)
+// Number of pre-computed primes for sieving.
+#define NPRIMES (ASIZE(primes))
+// Out of NCLASS, how many classes pass acceptClass().
 #define NGOODCLASS (2 * 2 * 4 * 6 * 10)
 
-#define SIEVE_THREADS 32
-#define WORK_THREADS (THREADS_PER_BLOCK - SIEVE_THREADS)
-#define NWORDS (12 * 1024)
-#define NBITS (NWORDS << 5)
-#define NPRIMES (ASIZE(primes))
-
-// whether 2 * c * p + 1 == 1 or 7 modulo 8.
-bool q1or7mod8(unsigned exp, u64 c) {
+// Returns whether 2 * c * exp + 1 is 1 or 7 modulo 8.
+// Any Marsenne factor must be of this form. See http://www.mersenne.org/various/math.php
+bool q1or7mod8(u32 exp, u32 c) {
   return !(c & 3) || ((c & 3) + (exp & 3) == 4);
 }
 
-// whether 2 * k * p + 1 != 0 modulo prime
-bool notMultiple(unsigned exp, unsigned c, unsigned prime) {
-  return !c || (2 * c * (u64) exp + 1) % prime;
+// whether 2 * c * exp + 1 != 0 modulo prime
+bool notMultiple(u32 exp, u32 c, unsigned prime) {
+  return (2 * c * (u64) exp + 1) % prime;
 }
 
-bool acceptClass(unsigned exp, unsigned c) {
+bool acceptClass(u32 exp, u32 c) {
+  // Keep in sync with NCLASS
   return q1or7mod8(exp, c) && notMultiple(exp, c, 3) && notMultiple(exp, c, 5)
     && notMultiple(exp, c, 7) && notMultiple(exp, c, 11);
 }
@@ -51,8 +89,12 @@ __device__ const u32 primes[] = {
 #include "primes-512k.inc"
 };
 
-__managed__ u64 foundFactor;
-__managed__ u16 classTab[NGOODCLASS];
+__managed__ u64 foundFactor;  // If a factor k is found, save it here.
+__managed__ u16 classTab[NGOODCLASS];  // The class value for each "good" class.
+
+// BTC means "bit to clear", the first bit position to clear in the big bit block when sieving.
+// Keeps the BTC for each prime, for each good class. The BTC is updated during sieving.
+// This is the bulk of the GPU [global] memory usage.
 __device__ u32 classBtcTab[NGOODCLASS][NPRIMES];
 
 // Funnel shift left.
@@ -116,13 +158,27 @@ __device__ static U4 sub(U4 x, U4 y) {
   return (U4) {a, b, c, d};
 }
 
-// return x - (y << 32)
+// returns x - (y << 32)
 __device__ U5 subShl1w(U5 x, U4 y) {
   U4 t = sub((U4) {x.b, x.c, x.d, x.e}, y);
   return (U5) {x.a, t.a, t.b, t.c, t.d};
 }
 
-__device__ static U4 mul(U3 x, u32 n) {
+// returns x * n; 4 MULs.
+__device__ U3 mul(U2 x, u32 n) {
+  u32 a, b, c;
+  asm(
+      "mul.hi.u32     %1, %3, %5;"
+      "mul.lo.u32     %0, %3, %5;"
+      "mad.lo.cc.u32  %1, %4, %5, %1;"
+      "madc.hi.u32    %2, %4, %5, 0;"
+      : "=r"(a), "=r"(b), "=r"(c)
+      : "r"(x.a), "r"(x.b), "r"(n));
+  return (U3) {a, b, c};
+}
+
+// returns x * n; 6 MULs.
+__device__ U4 mul(U3 x, u32 n) {
   u32 a, b, c, d;
   asm(
       "mul.hi.u32     %1, %4, %7;"
@@ -136,7 +192,7 @@ __device__ static U4 mul(U3 x, u32 n) {
   return (U4) {a, b, c, d};
 }
 
-// returns (x*n >> 32) + (n ? 1 : 0)
+// returns (x*n >> 32) + (n ? 1 : 0). Used for Montgomery reduction. 5 MULs.
 __device__ U3 mulM(U3 x, u32 n) {
   u32 a, b, c;
   asm(
@@ -149,19 +205,8 @@ __device__ U3 mulM(U3 x, u32 n) {
   return (U3) {a, b, c};
 }
 
-__device__ static U3 mul(U2 x, u32 n) {
-  u32 a, b, c;
-  asm(
-      "mul.hi.u32     %1, %3, %5;"
-      "mul.lo.u32     %0, %3, %5;"
-      "mad.lo.cc.u32  %1, %4, %5, %1;"
-      "madc.hi.u32    %2, %4, %5, 0;"
-      : "=r"(a), "=r"(b), "=r"(c)
-      : "r"(x.a), "r"(x.b), "r"(n));
-  return (U3) {a, b, c};
-}
-
-__device__ static U4 square(U2 x) {
+// returns x * x; 6 MULs.
+__device__ U4 square(U2 x) {
   u32 a, b, c, d;
   asm(
       "mul.lo.u32     %1, %4, %5;"
@@ -179,7 +224,8 @@ __device__ static U4 square(U2 x) {
   return (U4) {a, b, c, d};
 }
 
-__device__ static U6 square(U3 x) {
+// returns x * x; 12 MULs.
+__device__ U6 square(U3 x) {
   U2 ab = {x.a, x.b};
   U4 ab2 = square(ab);
   U3 abc = mul(ab, x.c + x.c);
@@ -197,28 +243,18 @@ __device__ static U6 square(U3 x) {
   return (U6) {ab2.a, ab2.b, c, d, e, f};
 }
 
-__device__ U5 shl1w(U4 x)  { return (U5) {0, x.a, x.b, x.c, x.d}; }
-__device__ U6 shl2w(U4 x)  { return (U6) {0, 0, x.a, x.b, x.c, x.d}; }
 __device__ U6 makeU6(U3 x) { return (U6) {x.a, x.b, x.c, 0, 0, 0}; }
 __device__ U5 makeU5(U4 x) { return (U5) {x.a, x.b, x.c, x.d, 0}; }
 __device__ U6 makeU6(U5 x) { return (U6) {x.a, x.b, x.c, x.d, x.e, 0}; }
 __device__ U6 makeU6(U4 x) { return makeU6(makeU5(x)); }
 __device__ U2 makeU2(u64 x) { return (U2) {(u32) x, (u32) (x >> 32)}; }
-__device__ U3 negative(U3 x) {
-  return (U3) {-x.a, ~x.b + (!x.a), ~x.c + (!x.a && !x.b)};
-}
-__device__ bool ge(U3 x, U3 y) {
-  return (x.c > y.c)
-    || (x.c == y.c && x.b > y.b)
-    || (x.c == y.c && x.b == y.b && x.a >= y.a);
-}
 
-__device__ static U3 shl(U3 x, int n) {
+__device__ U3 shl(U3 x, int n) {
   assert(n >= 0 && n < 32 && !(x.c >> (32 - n)));
   return (U3) {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n)};
 }
 
-__device__ static U4 shl(U4 x, int n) {
+__device__ U4 shl(U4 x, int n) {
   // assert(n >= 0 && n < 32 && !(x.d >> (32 - n)));
   return (U4) {x.a << n, shl(x.a, x.b, n), shl(x.b, x.c, n), shl(x.c, x.d, n)};
 }
@@ -233,10 +269,12 @@ __device__ U3 mod(U5 x, U3 m) {
   assert(m.c);
   int shift = __clz(m.c) + 1;
   assert(shift >= 18);
+  // R is an approximation from below of 2^64 / (m << shift).
   u32 R = 0xffffffffffffffffULL / ((0x100000000ULL | shl(m.b, m.c, shift)) + 1);
+  // A faster but lower-quality approximation of the same.
   // u32 R = 0x7fffffff00000001ULL / shl(m.b, m.c, shift - 1);
-  u32 n;
   
+  u32 n;  
   n = mulhi(x.e, R);
   x = subShl1w(x, shl(mul(m, n), shift));
   assert(!(x.e & 0xfffffff0));
@@ -257,7 +295,7 @@ __device__ U3 mod(U5 x, U3 m) {
 
 // Compute m' such that: (u32) (m * m') == 0xffffffff, using extended binary euclidian algorithm.
 // See http://www.ucl.ac.uk/~ucahcjm/combopt/ext_gcd_python_programs.pdf
-// m must be odd.
+// m is odd.
 __device__ static u32 mprime(u32 m) {
   m = (m >> 1) + 1;
   u32 u = m;
@@ -326,16 +364,19 @@ __device__ u32 modInv32(u64 step, u32 prime) {
 
 #define ID (blockIdx.x * blockDim.x + threadIdx.x)
 
-// 3 times 64bit modulo, very expensive!
+// 3 times 64bit modulo, expensive!
 __device__ int bitToClear(u32 exp, u64 k, u32 prime, u32 inv) {
   u32 kmod = k % prime;
   u32 qmod = (kmod * (u64) (exp << 1) + 1) % prime;
   return (prime - qmod) * (u64) inv % prime;
 }
 
+// Returns the position of the most significant bit that is set.
 __device__ int bfind(u32 x) { int r; asm("bfind.u32 %0, %1;": "=r"(r): "r"(x)); return r; }
 
-__global__ void __launch_bounds__(1024) initBtcTab(u32 exp, u64 k0, u64 step) {
+// Initializes the classBtcTab array in GPU memory.
+__global__ void __launch_bounds__(1024) initBtcTab(u32 exp, u64 k0) {
+  const u64 step = 2 * NCLASS * (u64) exp;
   for (const u32 *p = primes + ID, *end = primes + ASIZE(primes); p < end; p += gridDim.x * blockDim.x) {
     u32 prime = *p;
     u32 inv = modInv32(step, prime);
@@ -451,7 +492,6 @@ int main() {
   CUDA_CHECK_ERR;
   
   const u32 exp = 119904229;
-  const u64 step = 2 * NCLASS * (u64) exp;
   int startPow2 = 67;
   const u64 k0   = calculateK(exp, startPow2);
   const u64 kEnd = calculateK(exp, startPow2 + 1);
@@ -462,7 +502,7 @@ int main() {
 #define BTC_BLOCKS (NPRIMES / BTC_THREADS)
   assert(NPRIMES % BTC_THREADS == 0);
   u64 t1 = timeMillis();
-  initBtcTab<<<BTC_BLOCKS, BTC_THREADS>>>(exp, k0, step);
+  initBtcTab<<<BTC_BLOCKS, BTC_THREADS>>>(exp, k0);
   cudaDeviceSynchronize();
   u64 t2 = timeMillis();
   printf("initBtcTab: blocks %lu time %llu\n", BTC_BLOCKS, (t2 - t1));
