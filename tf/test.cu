ef8303dd33508d0eb6305dd077d5f82e53df1516
Mihai Preda <mhpreda@gmail.com>
2015-06-20 18:59:42 +1000

    remove old code, before streams

--- tf/test.cu
+++ tf/test.cu
@@ -245,11 +245,13 @@ __global__ void __launch_bounds__(1024) initBtcTab(u32 exp, u64 k) {
 // Returns the position of the most significant bit that is set.
 __device__ int bfind(u32 x) { int r; asm("bfind.u32 %0, %1;": "=r"(r): "r"(x)); return r; }
 
-__device__ u32 kTab[128 * 1024 * 1024];
+__device__ u32 kTab[160000000];
 __managed__ u32 kTabSize;
 
 #define N 64
 __global__ void __launch_bounds__(512, 2) test(u32 doubleExp, u32 flushedExp, u64 k) {
+  int n = kTabSize;
+  kTabSize = 0;
   int id = blockIdx.x * blockDim.x * N + threadIdx.x;
   for (int i = 0; i < N; ++i, id += blockDim.x) {
     u32 delta = kTab[id];
@@ -262,10 +264,10 @@ __global__ void __launch_bounds__(512, 2) test(u32 doubleExp, u32 flushedExp, u6
   }
 }
 
-//
-__global__ void __launch_bounds__(512, 4) sieve() {
+// __launch_bounds__(512, 2)
+__global__ void  sieve() {
   __shared__ u32 words[NWORDS];
-  // kTabSize = 0;
+  for (int rep = 32; rep; --rep) {
   const int tid = threadIdx.x;
   for (int i = 0; i < NWORDS / SIEVE_THREADS; ++i) { words[tid + i * SIEVE_THREADS] = 0; }
   __syncthreads();
@@ -326,7 +328,8 @@ __global__ void __launch_bounds__(512, 4) sieve() {
     bits &= ~(1 << bit);
     kTab[p++] = delta + bit;
   }
- out: return;
+  out:;
+  }
 }
 
 int classTab[NGOODCLASS];
@@ -343,7 +346,6 @@ void initClasses(u32 exp) {
 
 u64 calculateK(u32 exp, int bits) {
   return (((u128) 1) << (bits - 1)) / exp;
-  // return k - k % NCLASS;
 }
 
 #define CUDA_CHECK_ERR  err = cudaGetLastError(); if (err) { printf("CUDA error: %s\n", cudaGetErrorString(err)); return 0; }
@@ -388,103 +390,15 @@ int main() {
     printf("kTab %d\n", kTabSize);
     kTabSize = 0;
     // if (foundFactor) { printf("Factor K: %llu\n", foundFactor); break; }
-    if (!(cid & 0xf)) {
+    // if (!(cid & 0xf)) {
       u64 t2 = timeMillis();
       printf("%5d: class %5d: %llu\n", cid, c, t2 - t1);
       t1 = t2;
-    }
-    sieve<<<32 * 4 * 12 * 1024 / NWORDS, SIEVE_THREADS>>>();
+      // }
+    sieve<<<128 * 6 * 1024 / NWORDS, SIEVE_THREADS>>>();
     // cudaDeviceSynchronize();
     // test<<<(kTabSize + 512*N - 1) / (512*N), 512>>>(exp + exp, flushedExp, k);
   }
   printf("Total time: %llu ms\n", timeMillis() - t0);
   // cudaDeviceReset();
 }
-
-
-/*
-    // u64 kWord = k0 + c + (tid + round * NWORDS) * (32 * NCLASS);
-    // tid * (32 * NCLASS) + round * NCLASS * (u64) NBITS;
-
-__global__ void test(u32 exp, U3 m) {
-  u32 flushedExp = exp << __clz(exp);
-  isFactor(flushedExp, m);
-}
-
-__global__ void __launch_bounds__(SIEVE_THREADS, 2) sieve(u32 exp, u64 k0, int c0) {
-  __shared__ u32 words[NWORDS];
-  const int tid = threadIdx.x;
-  const int cid = c0 + blockIdx.x;
-  const int c = classTab[cid];
-  const u32 flushedExp = exp << __clz(exp);
-  u64 kBlock = k0 + c + (tid - SIEVE_THREADS) * (32 * NCLASS) - NBITS * (u64) NCLASS;
-  bool shouldExit = false;
-  // u32 timeSieve, timeTest, timeCopy;
-
-  for (int round = 0; round < nRounds && !shouldExit; ++round) {
-    // u32 time0 = clock();
-    if (tid < SIEVE_THREADS) {
-      // u32 *btcp = btcTab + tid;
-      for (const u32 *p = primes + tid, *end = primes + NPRIMES, *invp = invTab; p < end;
-           p += SIEVE_THREADS, invp += SIEVE_THREADS) {
-        int prime = *p;
-        int btc = bitToClear(exp, k0 + c, prime, *invp);
-        // int btc = *btcp;
-        while (btc < NBITS) {
-          atomicOr(words + (btc >> 5), 1 << (btc & 0x1f));
-          btc += prime;
-        }
-        // *btcp = btc - NBITS;
-      }
-      // timeSieve = clock() - time0;
-    } else {
-}
-
-#ifndef NDEBUG
-  U3 r2 = expMod2(flushedExp, q);
-  if (!(r.a == r2.a && r.b == r2.b && r.c == r2.c)) {
-    printf("%08x%08x%08x %08x%08x%08x %08x%08x%08x\n", r.c, r.b, r.a, r2.c, r2.b, r2.a, q.c, q.b, q.a);
-  }
-  assert(r.a == r2.a && r.b == r2.b && r.c == r2.c);
-#endif
-
-  
-#ifndef NDEBUG
-__device__ U3 mod(U5 x, U3 m) {
-  assert(m.c && !(m.c & 0xc0000000));
-  int sh = __clz(m.c) + 1;
-  if (sh > 26) {
-    m = shl(m, sh - 26);
-    sh = 26;
-  }
-  assert(sh >= 3 && sh <= 26);
-  u32 R = 0xffffffffffffffffULL / ((0x100000000ULL | shl(m.b, m.c, sh)) + 1);
-  u32 n = mulhi(x.e, R);
-  U4 t = sub((U4) {x.b, x.c, x.d, x.e}, shl(mul(m, n), sh));
-  x = (U5){x.a, t.a, t.b, t.c, t.d};
-  assert(!(x.e & 0xfffffff8));
-  n = mulhi(shl(x.d, x.e, 29), R);
-  U5 mn = shl(_U5(mul(m, n)), sh + 3);
-  x = sub(x, mn);
-  assert(!x.e && !(x.d & 0xffffffc0));
-  n = mulhi(shl(x.c, x.d, 26), R) >> (26 - sh);
-  t = sub((U4) {x.a, x.b, x.c, x.d}, mul(m, n));
-  assert(!t.d);
-  assert(!(t.c >> (35 - sh)));
-  return (U3) {t.a, t.b, t.c};
-}
-
-__device__ U3 expMod2(u32 exp, U3 m) {
-  assert(exp & 0x80000000);
-  int sh = exp >> 26;
-  assert(sh >= 32 && sh < 64);
-  U3 a = mod((U5){0, 0, 0, 0, 1 << (sh - 32)}, m);
-  u32 mp = mprime(m.a);
-  for (exp <<= 6; exp; exp += exp) {
-    a = montRed(square(a), m, mp);
-    if (exp & 0x80000000) { a = shl(a, 1); }
-  }
-  return montRed(_U6(a), m, mp);
-}
-#endif
-*/
