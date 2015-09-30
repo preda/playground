aed39f0668a38a8e9f45ee0320bed978f1e701a0
Mihai Preda <mhpreda@gmail.com>
2015-09-30 21:46:07 +1000

    positionBits

--- go/Go.cpp
+++ go/Go.cpp
@@ -262,6 +262,7 @@ struct Eval {
 
 u64 bensonAlive(u64 black, u64 white, const u8 *gids);
 bool canPlay(int pos, u64 black, u64 white);
+u64 stonesBase3(u64 stones);
 
 class Node {
   u64 black, white;
@@ -276,8 +277,12 @@ public:
   bool isKo() PURE { return koPos; }
   int nPass() PURE { return _nPass; }
   
-  u64 position() { return 0; }
-  u64 situationBits() { return 0; }
+  u64 positionBits() { return (stonesBase3(black) << 1) + stonesBase3(white); }
+  
+  u64 situationBits() {
+    assert(koPos >= 0 && _nPass >= 0);
+    return koPos | (_nPass << 6) | (swapped ? 0x100 : 0);
+  }
   
   Eval eval() PURE {
     return Eval{bensonAlive(black, white, gids), bensonAlive(white, black, gids)};
@@ -302,11 +307,49 @@ private:
   void playAux(int pos);
   void playNotPass(int pos);
   void swap() { std::swap(black, white); swapped != swapped; }
-  
-  bool isWhite(int p) const { return p >= 0 && IS(p, white); }
-  // bool isEmpty(int p) const { return isInside(p) && ~IS(p, black | white); }
-  // u64 capture(int p) const;
 };
+
+union Bytes {
+  u64 value;
+  u8 bytes[8];
+  unsigned operator[](int p) { return bytes[p]; }
+};
+
+u64 extract(u64 stones) {
+  Bytes b{.value = stones};
+  return b[0] | (b[1] << 6) | (b[2] << 12) | (b[3] << 18) | (b[4] << 24) |
+    (((u64) b[5]) << 30);
+}
+
+int tab3[256] = {
+   0,    1,    3,    4,    9,   10,   12,   13,   27,   28,   30,   31,   36,   37,   39,   40,
+  81,   82,   84,   85,   90,   91,   93,   94,  108,  109,  111,  112,  117,  118,  120,  121,
+ 243,  244,  246,  247,  252,  253,  255,  256,  270,  271,  273,  274,  279,  280,  282,  283,
+ 324,  325,  327,  328,  333,  334,  336,  337,  351,  352,  354,  355,  360,  361,  363,  364,
+ 729,  730,  732,  733,  738,  739,  741,  742,  756,  757,  759,  760,  765,  766,  768,  769,
+ 810,  811,  813,  814,  819,  820,  822,  823,  837,  838,  840,  841,  846,  847,  849,  850,
+ 972,  973,  975,  976,  981,  982,  984,  985,  999, 1000, 1002, 1003, 1008, 1009, 1011, 1012,
+1053, 1054, 1056, 1057, 1062, 1063, 1065, 1066, 1080, 1081, 1083, 1084, 1089, 1090, 1092, 1093,
+2187, 2188, 2190, 2191, 2196, 2197, 2199, 2200, 2214, 2215, 2217, 2218, 2223, 2224, 2226, 2227,
+2268, 2269, 2271, 2272, 2277, 2278, 2280, 2281, 2295, 2296, 2298, 2299, 2304, 2305, 2307, 2308,
+2430, 2431, 2433, 2434, 2439, 2440, 2442, 2443, 2457, 2458, 2460, 2461, 2466, 2467, 2469, 2470,
+2511, 2512, 2514, 2515, 2520, 2521, 2523, 2524, 2538, 2539, 2541, 2542, 2547, 2548, 2550, 2551,
+2916, 2917, 2919, 2920, 2925, 2926, 2928, 2929, 2943, 2944, 2946, 2947, 2952, 2953, 2955, 2956,
+2997, 2998, 3000, 3001, 3006, 3007, 3009, 3010, 3024, 3025, 3027, 3028, 3033, 3034, 3036, 3037,
+3159, 3160, 3162, 3163, 3168, 3169, 3171, 3172, 3186, 3187, 3189, 3190, 3195, 3196, 3198, 3199,
+3240, 3241, 3243, 3244, 3249, 3250, 3252, 3253, 3267, 3268, 3270, 3271, 3276, 3277, 3279, 3280,
+};
+
+#define POW8 6561
+#define POW16 43046721ull
+#define POW24 (POW16 * POW8)
+#define POW32 (POW16 * POW16)
+u64 base3(u64 bits) {
+  Bytes b{.value = bits};
+  return tab3[b[0]] + POW8*tab3[b[1]] + POW16*tab3[b[2]] + POW24*tab3[b[3]]  + POW32*tab3[b[4]];
+}
+
+u64 stonesBase3(u64 stones) { return base3(extract(stones)); }
 
 // returns captured *black* group at pos.
 u64 capture(int pos, u64 black, u64 white) {
@@ -326,6 +369,16 @@ u64 capture(int pos, u64 black, u64 white) {
   }
 }
 
+bool canPlay(int pos, u64 black, u64 white) {
+  assert(pos >= 0 && IS(pos, INSIDE));
+  if (IS(pos, black | white)) { return false; }
+  SET(pos, black);
+  for (int p : NEIB(pos)) {
+    if (p >= 0 && IS(p, white) && capture(p, white, black)) { return true; }
+  }
+  return !capture(pos, black, white);
+}
+
 // set gids for the black group at pos.
 void Node::setGroup(int pos, int gid) {
   assert(pos >= 0 && IS(pos, black));
@@ -339,16 +392,6 @@ void Node::setGroup(int pos, int gid) {
     if (!open) { break; }
     pos = POP(open);
   }
-}
-
-bool canPlay(int pos, u64 black, u64 white) {
-  assert(pos >= 0 && IS(pos, INSIDE));
-  if (IS(pos, black | white)) { return false; }
-  SET(pos, black);
-  for (int p : NEIB(pos)) {
-    if (p >= 0 && IS(p, white) && capture(p, white, black)) { return true; }
-  }
-  return !capture(pos, black, white);
 }
 
 void Node::playNotPass(int pos) {
@@ -513,7 +556,7 @@ u64 bensonAlive(u64 black, u64 white, const u8 *gids) {
 Transtable tt;
 
 Value max(History history, Node node, int k, int depthPos, int maxDepth) {
-  u64 position  = node.position();
+  u64 position  = node.positionBits();
   u64 sbits = node.situationBits();
   u128 situation = (((u128) sbits) << 64) | position;
   bool isPlain = !node.isKo() && !node.nPass();
@@ -528,7 +571,6 @@ Value max(History history, Node node, int k, int depthPos, int maxDepth) {
 
   Eval eval = node.eval();
   v = eval.value(k);
-  // v = node.eval(k);
   if (v.isFinalEnough(k) || depthPos >= maxDepth) { return v; }
 
   history.push(situation);
